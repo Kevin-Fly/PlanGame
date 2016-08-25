a961b435497324d50c38d40f7d2039abe7aaf3f8
tengye <843203996@qq.com>
2016-08-25 10:46:46 +0800

    add bullet

--- js/plan.js
+++ js/plan.js
@@ -23,6 +23,13 @@ var playBtn = document.getElementById('playBtn');
 
 var gameState = 0; 	// 0:游戏未开始或游戏结束     1：正在游戏
 var score = 0; 	// 分数
+var bulletSpeed = 20; //子弹的速度
+
+var mapw = map.offsetWidth;
+var maph = map.offsetHeight;
+
+var Bullets = [];
+
 
 
 // 开始游戏
@@ -34,22 +41,92 @@ playBtn.onclick = function(){
 // 重新玩
 restartBtn.onclick = function(){
 	gameState = 1;
+	score = 0;
 	gameScore.innerHTML = score;
 	gameOverPage.style.display = 'none';
+	//重新创建一个飞机,并设置位置
 	hero = document.createElement('img');
 	hero.src = 'img/hero.png';
 	map.appendChild(hero);
-	hero.style.height = '60px'
-	hero.style.width = '80px'
-	hero.style.left = '240px'
-	hero.style.top = '500px'
-	hero.style.position = 'absolute'
-	
-	
-	
+	hero.style.height = '60px';
+	hero.style.width = '80px';
+	hero.style.left = '240px';
+	hero.style.top = '500px';
+	hero.style.position = 'absolute';
+
+	fire = setInterval(addBullet , 200);
+	updateTimer = setInterval(update , 30);
+
+}
+// 飞机随着鼠标移动
+map.onmousemove = function (e) {
+	var x = e.clientX;
+	var y = e.clientY;
+
+	//offsetWidth是指对象的可见宽度
+	//offsetWidth = width + padding + border
+	var w = hero.offsetWidth;
+	var h = hero.offsetHeight;
+
+	hero.style.left = x - w/2 +'px';
+	hero.style.top = y - h/2 +'px';
+
+	if(hero.offsetLeft < 0){
+		hero.style.left = '0px';
+	}
+	if(hero.offsetLeft > (mapw - w)){
+		hero.style.left = (mapw - w) +'px';
+	}
+	if(hero.offsetTop < 0){
+		hero.style.top = '0px';
+	}
+	if(hero.offsetTop > (maph - h)){
+		hero.style.right = (maph - h) + 'px';
+	}
+
 }
 
+// 飞机的子弹
+var fire = setInterval(addBullet , 200);
+function addBullet() {
+	if (gameState == 1){
+		var bullet = document.createElement('img');
+		bullet.src = 'img/bullet1.png';
+		bullet.style.position = 'absolute';
+		bullet.style.width = '20px';
 
+		var bx = hero.offsetLeft;
+		var by = hero.offsetTop;
+
+		bullet.style.left = (bx + hero.offsetWidth / 2 - 10)+'px';
+		bullet.style.top = by - 10 +'px';
+
+		map.appendChild(bullet);
+		Bullets.push(bullet);
+
+	}
+}
+
+var updateTimer = setInterval(update , 30);
+function update() {
+	if (gameState == 1){
+
+		for(var i = 0 ; i < Bullets.length ; i++){
+
+			var b = Bullets[i];
+
+			b.style.top = b.offsetTop - bulletSpeed +'px';
+
+			if (b.offsetTop < 0){
+				map.removeChild(b);
+				// 从第i个位置开始删除一个元素
+				Bullets.splice(i , 1);
+			}
+
+		}
+
+	}
+}
 
 
 
